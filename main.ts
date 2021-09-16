4e2ddd58bbf37d7957ba0abe936f58e744d18b52
leondublin <16LCummins@stjosephsrush.com>
2021-09-16 12:53:20 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (light2 >= 70) {
+        basic.showString("Lights Off")
+    } else {
+        basic.showString("Lights On")
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    if (light2 < 70) {
+        basic.showString("Lights On")
+    } else {
+        basic.showString("Lights Off")
+    }
+})
+let light2 = 0
+basic.showString("Hi!")
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+basic.showString("A or B?")
